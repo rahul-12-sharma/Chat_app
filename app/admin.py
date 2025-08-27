64c68c30473276f32c0f66e47e1348d9be18bb03
Rahul Sharma <rg4142451@gmail.com>
2025-08-27 21:23:10 +0530

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+
+admin.site.register(Room)
+admin.site.register(Message)
+
+
+
+## admin name = rahulchat , pass = 123
---
+++ app/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render , redirect 
+from django.http import Http404 , HttpResponse , JsonResponse
+from .models import *
+
+def home(request):
+    return render(request , 'home.html')
+
+
+def room(request , room):
+    username = request.GET.get('username')
+
+    try:
+        room_details = Room.objects.get(name=room)
+    except Room.DoesNotExist:
+        raise Http404("Room does not exist")
+
+
+    return render(request , 'room.html' , {
+        'username' : username,
+        'room' : room,
+        'room_details' : room_details
+    })
+
+
+def checkview(request ):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/' + room + '/?username=' + username)
+    
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/' + room + '/?username=' + username )
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+
+def getMessages(request , room):
+    room_details = Room.objects.get(name=room)
+
+    message = Message.objects.filter(room=room_details.id)
+    return JsonResponse({
+        'messages' : list(message.values())
+        })
